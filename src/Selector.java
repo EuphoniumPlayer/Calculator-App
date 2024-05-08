10a4dd94c1fe91fb08f2b937ee602c8567aeb0e4
EuphoniumPlayer <chasewmanzella@gmail.com>
2024-05-07 21:35:15 -0500

    Added Functionality

--- src/Selector.java
+++ src/Selector.java
@@ -2,46 +2,68 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Selector implements ActionListener{
+public class Selector implements ActionListener {
 
-    JFrame frame;
+    public JFrame frame;
     JTextField textfield;
     JButton calc, chem;
     JPanel buttonpanel;
 
-    Font font = new Font("Arial",Font.PLAIN,30);
+    Font font = new Font("Arial",Font.PLAIN,25);
 
     Selector () {
         
-        frame = new JFrame();
+        frame = new JFrame("Selection Menu");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(420, 800);
         frame.setLayout(null);
 
         textfield = new JTextField();
-        textfield.setBounds(50, 25, 300, 50);
+        textfield.setBounds(40, 300, 310, 50);
         textfield.setFont(font);
+        textfield.setText("Select your choice operation");
         textfield.setEditable(false);
+
+        buttonpanel = new JPanel();
+        buttonpanel.setBounds(40, 360, 310, 75);
+        buttonpanel.setLayout(new GridLayout(1, 2, 10, 10));
 
         calc = new JButton("Calculator");
         calc.addActionListener(this);
         calc.setFont(font);
         calc.setFocusable(false);
+        buttonpanel.add(calc);
+
         chem = new JButton("Chemistry");
         chem.addActionListener(this);
-        calc.setFont(font);
+        chem.setFont(font);
         chem.setFocusable(false);
+        buttonpanel.add(chem);
 
+        frame.add(buttonpanel);
+        frame.add(textfield);
+        frame.setVisible(true);
+    }
+
+    public void returned () {
         frame.setVisible(true);
     }
 
     private static Calculator calculator;
+    private static chemistry chemistry;
     public static void main(String[] args) {
         Selector sel = new Selector();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        if(e.getSource() == calc) {
+            calculator = new Calculator();
+            frame.setVisible(false);
+        }
+        if(e.getSource() == chem) {
+            chemistry = new chemistry();
+            frame.setVisible(false);
+        }
     }
 }
--- src/chemistry.java
+++ src/chemistry.java
@@ -1,3 +1,44 @@
-public class chemistry {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
+public class chemistry implements ActionListener {
+    JFrame frame;
+    JTextField textfield;
+    JButton returnbut;
+    //TODO: Add buttons and panel
+    Selector sel = new Selector();
+
+    chemistry () {
+
+        frame = new JFrame("Chemistry");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420, 800);
+        frame.setLayout(null);
+
+        textfield = new JTextField();
+        textfield.setBounds(40, 300, 310, 50);
+        textfield.setFont(new Font("Arial",Font.ITALIC,30));
+        textfield.setText("Under Construction");
+        textfield.setEditable(false);
+
+        returnbut = new JButton("Return");
+        returnbut.setBounds(40, 360, 310, 74);
+        returnbut.setFont(new Font("Arial",Font.BOLD,30));
+        returnbut.addActionListener(this);
+        returnbut.setFocusable(false);
+
+        frame.add(returnbut);
+        frame.add(textfield);
+        frame.setVisible(true);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == returnbut) {
+            sel.returned();
+            frame.setVisible(false);
+        }
+    }
 }
